25e17ae2325b5a29c2dbceda1c88bd8ba4dabe5a
Daniyar <daniyar.nurgaliyev@babel.es>
2020-07-28 21:13:28 +0200

    Refactored Base and ToggleMenu classes

--- app/src/test/java/com/dd/appiumtest/test/BaseTest.java
+++ app/src/test/java/com/dd/appiumtest/test/BaseTest.java
@@ -1,42 +1,45 @@
 package com.dd.appiumtest.test;
+
 import com.dd.appiumtest.pages.MainPage;
 
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.net.MalformedURLException;
 import java.net.URL;
+
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+
 class BaseTest {
 
     protected static final String APPIUM = "http://localhost:4723/wd/hub";
     protected static final String URL = "https://babel.es";
     protected static final Integer TIME_OUT_IN_SECONDS = 10;
     protected static final Integer SLEEP_IN_MILLIS = 1000;
+    private static final String PLATFORM_NAME = "Android";
+    private static final String PLATFORM_VERSION = "7";
+    private static final String DEVICE_NAME = "f2c4e47b";
+    private static final String AUTOMATION_NAME = "UiAutomator2";
+    private static final String BROWSER_NAME = "Chrome";
+    private static final String CHROME_DRIVER_EXECUTABLE_DIR = "/Users/daniyar.nurgaliyev.local/Downloads/appium_chromedriver_temp";
+    private static final String CHROME_DRIVER_CHROME_MAPPING_FILE = "/Users/daniyar.nurgaliyev.local/Downloads/appium_chromedriver_temp/chromedriverChromeMappingFile";
 
     protected RemoteWebDriver driver;
     protected WebDriverWait wait;
+    private DesiredCapabilities caps;
+    private MainPage mainPage;
 
     @Before
     public void setUp() throws Exception {
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("platformName", "Android");
-        caps.setCapability("platformVersion", "7");
-        caps.setCapability("deviceName", "f2c4e47b");
-        caps.setCapability("automationName", "UiAutomator2");
-        caps.setCapability("browserName", "Chrome");
-        caps.setCapability("chromedriverExecutableDir", "/Users/daniyar.nurgaliyev.local/Downloads/appium_chromedriver_temp");
-        caps.setCapability("chromedriverChromeMappingFile", "/Users/daniyar.nurgaliyev.local/Downloads/appium_chromedriver_temp/chromedriverChromeMappingFile");
-
-        driver = new RemoteWebDriver(new URL(APPIUM), caps);
-        wait = new WebDriverWait(driver, TIME_OUT_IN_SECONDS);
-
-        driver.get(URL);
-        Thread.sleep(SLEEP_IN_MILLIS);
-
-        MainPage mainPage = new MainPage(driver, wait);
-        mainPage.acceptCookies();
+        setupDesiredCapabilities();
+        setupObjects();
+        setupDriver();
+        acceptCookies();
     }
 
     @After
@@ -45,4 +48,34 @@ class BaseTest {
             driver.quit();
         }
     }
+
+    private void setupDesiredCapabilities() {
+        caps = new DesiredCapabilities();
+        caps.setCapability(CapabilityType.PLATFORM_NAME, PLATFORM_NAME);
+        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, PLATFORM_VERSION);
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, DEVICE_NAME);
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, AUTOMATION_NAME);
+        caps.setCapability(CapabilityType.BROWSER_NAME, BROWSER_NAME);
+        caps.setCapability(AndroidMobileCapabilityType.CHROMEDRIVER_EXECUTABLE_DIR, CHROME_DRIVER_EXECUTABLE_DIR);
+        caps.setCapability(AndroidMobileCapabilityType.CHROMEDRIVER_CHROME_MAPPING_FILE, CHROME_DRIVER_CHROME_MAPPING_FILE);
+    }
+
+    private void acceptCookies() {
+        mainPage.acceptCookies();
+
+    }
+
+    private void setupDriver() throws InterruptedException {
+        driver.get(URL);
+        Thread.sleep(SLEEP_IN_MILLIS);
+
+    }
+
+
+    private void setupObjects() throws MalformedURLException {
+        driver = new RemoteWebDriver(new URL(APPIUM), caps);
+        wait = new WebDriverWait(driver, TIME_OUT_IN_SECONDS);
+        mainPage = new MainPage(driver, wait);
+    }
+
 }
--- app/src/test/java/com/dd/appiumtest/test/ToggleMenuTest.java
+++ app/src/test/java/com/dd/appiumtest/test/ToggleMenuTest.java
@@ -6,26 +6,32 @@ import org.junit.Test;
 
 public class ToggleMenuTest extends BaseTest {
 
+    private ToggleMenuPage toggleMenuPage;
     @Test
-    public void test() throws InterruptedException {
+    public void test() {
 
-        ToggleMenuPage toggleMenuPage = new ToggleMenuPage(driver, wait);
+        toggleMenuPage = new ToggleMenuPage(driver, wait);
 
         toggleMenuPage.open();
         toggleMenuPage.checkIfStartTextExist();
         toggleMenuPage.selectSectors();
+
         toggleMenuPage.open();
         toggleMenuPage.checkIfStartTextExist();
         toggleMenuPage.selectServices();
+
         toggleMenuPage.open();
         toggleMenuPage.checkIfStartTextExist();
         toggleMenuPage.selectPersons();
+
         toggleMenuPage.open();
         toggleMenuPage.checkIfStartTextExist();
         toggleMenuPage.selectMedia();
+
         toggleMenuPage.open();
         toggleMenuPage.checkIfStartTextExist();
         toggleMenuPage.selectContacts();
+
         toggleMenuPage.open();
         toggleMenuPage.checkIfStartTextExist();
         toggleMenuPage.close();
