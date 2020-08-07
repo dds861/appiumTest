79e70c2e8227cb0174b582e1b4463f631f11c84c
Daniyar <daniyar.nurgaliyev@babel.es>
2020-08-07 09:24:18 +0200

    toggle menu modified

--- app/src/test/java/com/dd/appiumtest/stepDefenitions/toggleMenu/MyAreaTestSteps.java
+++ app/src/test/java/com/dd/appiumtest/stepDefenitions/toggleMenu/MyAreaTestSteps.java
@@ -1,6 +1,8 @@
 package com.dd.appiumtest.stepDefenitions.toggleMenu;
 
 import com.dd.appiumtest.base.BaseTest;
+import com.dd.appiumtest.pages.MainPage;
+import com.dd.appiumtest.pages.SignInPage;
 import com.dd.appiumtest.pages.ToggleMenuPage;
 
 import java.net.MalformedURLException;
@@ -9,8 +11,14 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static com.dd.appiumtest.contants.Constants.TEXT_CORRECT_PASSWORD;
+import static com.dd.appiumtest.contants.Constants.TEXT_CORRECT_USERNAME;
+import static com.dd.appiumtest.contants.Constants.TEXT_INTRANET_MAIN_PAGE_TITLE;
+
 public class MyAreaTestSteps extends BaseTest {
     private ToggleMenuPage toggleMenuPage = new ToggleMenuPage(wait, androidDriver);
+    private SignInPage signInPage = new SignInPage();
+    private MainPage mainPage = new MainPage();
 
     public MyAreaTestSteps() throws MalformedURLException, InterruptedException {
         super();
@@ -18,12 +26,17 @@ public class MyAreaTestSteps extends BaseTest {
 
     @Given("The main page at MyAreaTest")
     public void the_main_page() {
-        toggleMenuPage.open();
+        androidDriver.get(URL);
+        signInPage.fillUsernameWithText(TEXT_CORRECT_USERNAME);
+        signInPage.fillPasswordWithText(TEXT_CORRECT_PASSWORD);
+        signInPage.clickSignInButton();
+        String pageTitle = mainPage.getPageTitleText();
+        assert pageTitle.equals(TEXT_INTRANET_MAIN_PAGE_TITLE);
     }
 
     @When("My Area clicked at MyAreaTest")
     public void my_area_clicked() {
-
+        toggleMenuPage.open();
     }
 
     @Then("{string} is seen at MyAreaTest")
